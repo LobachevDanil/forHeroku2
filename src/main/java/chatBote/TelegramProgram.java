0a3c368a0431e083c599f05cfe71e6ddd6e3b8b5
LobachevDanil <50016131+LobachevDanil@users.noreply.github.com>
2019-11-15 14:52:25 +0500

    init

---
+++ src/main/java/chatBote/TelegramProgram.java
@@ -0,0 +1,28 @@
+package chatBote;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import tasks_extractor.QuizTasksExtractor;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.io.File;
+
+public class TelegramProgram {
+    public static String questionPath = "src\\main\\resources\\questions";
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        /*File dir = new File(System.getProperty("user.dir"));
+        System.out.println(dir.getAbsolutePath());*/
+        QuizTasksExtractor extractor = new QuizTasksExtractor(questionPath);
+        TelegramBot bot = new TelegramBot();
+        TelegramBotLogic telegramBotLogic = new TelegramBotLogic(bot, extractor);
+        bot.subscribe(telegramBotLogic);
+        try {
+            TelegramBotsApi botapi = new TelegramBotsApi();
+            botapi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
